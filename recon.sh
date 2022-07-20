d0de125634a925c319be09993a680acefd6bab6d
root <mithatsarikaya58@gmail.com>
2022-07-20 03:28:15 -0400

    & added for parallel work

--- recon.sh
+++ recon.sh
@@ -21,15 +21,19 @@ touch "$NAME"/"$NAME".txt
 
 echo "$TARGET" >> "$NAME"/"$NAME".txt
 
+
+# "&" added for parallel work, going to fix output order, it seems messy now.
+
 #nmap  
-nmap -v -sS -sC -sV -O -p- -A -T4 $TARGET -oN "$NAME"/nmap_"$NAME".txt
+#-sS -sC -sV -O removed. looks like -A is enough for them.
+nmap -A -v -p- -T4 $TARGET -oN "$NAME"/nmap_"$NAME".txt &
 
 #-r added. dont wanna check directories content
 #dirb
-dirb http://"$TARGET" -r -o "$NAME"/dirb_"$NAME".txt
+dirb http://"$TARGET" -r -o "$NAME"/dirb_"$NAME".txt &
 
 
 #nikto
-nikto -h "$TARGET" -o "$NAME"/nikto_"$NAME".txt
+nikto -h "$TARGET" -o "$NAME"/nikto_"$NAME".txt &
 
 
