08c89aac28157fb66dff7a619a40cc0b113e6d79
root <mithatsarikaya58@gmail.com>
2022-07-23 05:17:33 -0400

    big update, used getopts, make dirb and nikto optional

--- recon.sh
+++ recon.sh
@@ -1,19 +1,68 @@
 #!/bin/bash
 
+#todo : script name will be fixed in usage function
+
 #auotomate nmap and dirb and create related folder and files
-#just for githubcheck
 
-TARGET=$1
-NAME=$2
 
-echo $TARGET
-echo $NAME
+usage(){
+	echo '*****************************************************************'
+	echo "Usage: ${0} [-nd] [-t <IP>] [-f <name>] "
+	echo '	*scanning nmap, creating folder and txt files, dirb and nikto are optional'
+	echo '	*Give ip after -t'
+	echo '	*Give name after -n'
+	echo "	*eg : ${0} -t 192.168.1.1 -n Home"
+	echo '*****************************************************************'
+	exit 1
+	}
+
+
+if [ "${#}" -eq 0 ]
+then
+	echo "You need to give parameters"
+	usage
+	exit 1
+fi
+
+
+while getopts t:f:nd OPTION
+do
+	case ${OPTION} in
+		t)
+			TARGET="${OPTARG}"
+			echo "Target is $TARGET"
+			;;
+		f)
+			NAME="${OPTARG}"
+			echo "name is $NAME" 
+			;;
+		n)
+			NIKTO='true'
+			echo "Nikto is on"
+			;;
+		d)
+			DIRB='true'
+			echo "Dirb is on"
+			;;
+		?)
+			usage
+			;;
+	esac
+done
+
+#TARGET=$1
+#NAME=$2
+
+#echo $TARGET
+#echo $NAME
 
 #check if the file exists. if exists end the script with exit status of 1
 if [ ! -d "$NAME" ]
-then	mkdir "$NAME"
-else	echo ""$NAME" directory exist, check it out"
-exit 1
+then	
+	mkdir "$NAME"
+else	
+	echo "$NAME directory exist, check it out"
+	exit 1
 fi
 
 #create main txt
@@ -26,14 +75,25 @@ echo "$TARGET" >> "$NAME"/"$NAME".txt
 
 #nmap  
 #-sS -sC -sV -O removed. looks like -A is enough for them.
-nmap -A -v -p- -T4 $TARGET -oN "$NAME"/nmap_"$NAME".txt &
+#nmap -A -v -p- -T4 $TARGET -oN "$NAME"/nmap_"$NAME".txt &
+
+nmap -sC -sV -T5 $TARGET -oN "$NAME"/nmap_"$NAME".txt
 
 #-r added. dont wanna check directories content
 #dirb
-dirb http://"$TARGET" -r -o "$NAME"/dirb_"$NAME".txt &
 
+if [[ $DIRB = 'true' ]]
+then
+	dirb http://"$TARGET" -r -o "$NAME"/dirb_"$NAME".txt 
+fi
 
 #nikto
-nikto -h "$TARGET" -o "$NAME"/nikto_"$NAME".txt &
+
+if [[ $NIKTO = 'true' ]]
+then
+	nikto -h "$TARGET" -o "$NAME"/nikto_"$NAME".txt 
+fi
+
+
 
 
