4d35867d80683702abb700253741468a75b84b6d
root <mithatsarikaya58@gmail.com>
2022-07-26 04:53:39 -0400

    gobuster added.

--- recon.sh
+++ recon.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #todo : script name will be fixed in usage function
+#todo : add gobuster instead of dirb.  +done
+#todo : add "export IP=$TARGET" to the $NAME.txt   +done
+
+
+
 
 #auotomate nmap and dirb and create related folder and files
 
@@ -10,7 +15,8 @@ usage(){
 	echo "Usage: ${0} [-nd] [-t <IP>] [-f <name>] "
 	echo '	*scanning nmap, creating folder and txt files, dirb and nikto are optional'
 	echo '	*Give ip after -t'
-	echo '	*Give name after -n'
+	echo '	*Give name after -f'
+	echo '	*-n for nikto, -d for dirb, -g for gobuster'
 	echo "	*eg : ${0} -t 192.168.1.1 -n Home"
 	echo '*****************************************************************'
 	exit 1
@@ -25,7 +31,7 @@ then
 fi
 
 
-while getopts t:f:nd OPTION
+while getopts t:f:ndg OPTION
 do
 	case ${OPTION} in
 		t)
@@ -44,17 +50,16 @@ do
 			DIRB='true'
 			echo "Dirb is on"
 			;;
+		g)
+			GOBUSTER='true'
+			echo "Gobuster is on"
+			;;
 		?)
 			usage
 			;;
 	esac
 done
 
-#TARGET=$1
-#NAME=$2
-
-#echo $TARGET
-#echo $NAME
 
 #check if the file exists. if exists end the script with exit status of 1
 if [ ! -d "$NAME" ]
@@ -68,7 +73,8 @@ fi
 #create main txt
 touch "$NAME"/"$NAME".txt
 
-echo "$TARGET" >> "$NAME"/"$NAME".txt
+#for easy copy and paste tasks
+echo "export IP=$TARGET" >> "$NAME"/"$NAME".txt
 
 
 # "&" added for parallel work, going to fix output order, it seems messy now.
@@ -77,7 +83,8 @@ echo "$TARGET" >> "$NAME"/"$NAME".txt
 #-sS -sC -sV -O removed. looks like -A is enough for them.
 #nmap -A -v -p- -T4 $TARGET -oN "$NAME"/nmap_"$NAME".txt &
 
-nmap -sC -sV -T5 $TARGET -oN "$NAME"/nmap_"$NAME".txt
+nmap -sC -sV -sS -T5 $TARGET -oN "$NAME"/nmap_"$NAME".txt
+
 
 #-r added. dont wanna check directories content
 #dirb
@@ -86,6 +93,13 @@ if [[ $DIRB = 'true' ]]
 then
 	dirb http://"$TARGET" -r -o "$NAME"/dirb_"$NAME".txt 
 fi
+
+#gobuster
+if [[ $GOBUSTER = 'true' ]]
+then
+	gobuster dir -u http://"$TARGET" -w /usr/share/dirb/wordlists/common.txt -x "php,txt,html" -o "$NAME"/gobust_"$NAME".txt
+fi
+
 
 #nikto
 
